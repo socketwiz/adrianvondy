afb5a950d6dff391156f66abccd51397cc53064d
Ricky Nelson <socketwiz@Rickys-iMac.local>
2011-11-14 19:57:48 -0500

    forgot to add the titles, keywords and descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Adrian & Vondy Law Firm in Winchester, Virginia"
+    if @title.nil?
+      base_title
+    else
+      "#{@title}"
+    end
+  end
+
+  def description
+    base_description = nil
+    if @description.nil?
+      base_description
+    else
+      "<meta name=\"description\" content=\"#{@description}\" />"
+    end
+  end
+
+  def keywords
+    base_keywords = nil
+    if @keywords.nil?
+      base_keywords
+    else
+      "<meta name=\"keywords\" content=\"#{@keywords}\" />"
+    end
+  end
 end
