7849c83507bfae6cc68f5cca41893ed4457e8d58
Ricky Nelson <rickyn@socketwiz.com>
2019-07-13 08:07:46 -0400

    Updates

---
+++ precache-manifest.53685cf8345d92bbd4200f4660e7b01a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9089abd5d4c49ed04f28f09079f0a542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc3bf005f20f6df47fcc",
+    "url": "/static/css/main.40d655d2.chunk.css"
+  },
+  {
+    "revision": "62264243012ec146b76f",
+    "url": "/static/js/2.114cd458.chunk.js"
+  },
+  {
+    "revision": "cc3bf005f20f6df47fcc",
+    "url": "/static/js/main.b9ef710d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04b96724cfc3a76565840b949c6205da",
+    "url": "/static/media/TitilliumText22L004-webfont.04b96724.svg"
+  },
+  {
+    "revision": "2454057f5e16fab4f9e424883f214aad",
+    "url": "/static/media/TitilliumText22L004-webfont.2454057f.ttf"
+  },
+  {
+    "revision": "4755171b1c25207f9d66c9f44f92f2b8",
+    "url": "/static/media/TitilliumText22L004-webfont.4755171b.woff"
+  },
+  {
+    "revision": "a95f3487c6e135ed50df2980950f0b5b",
+    "url": "/static/media/TitilliumText22L004-webfont.a95f3487.eot"
+  },
+  {
+    "revision": "07416bbdae60344815e7473548db5589",
+    "url": "/static/media/TitilliumText22L005-webfont.07416bbd.ttf"
+  },
+  {
+    "revision": "2eab4770cc6e53926aee2d83941f787e",
+    "url": "/static/media/TitilliumText22L005-webfont.2eab4770.eot"
+  },
+  {
+    "revision": "443d97e1fae56a989112a002879054fe",
+    "url": "/static/media/TitilliumText22L005-webfont.443d97e1.woff"
+  },
+  {
+    "revision": "8c545ddc741158d109a7d9d7066f85c5",
+    "url": "/static/media/TitilliumText22L005-webfont.8c545ddc.svg"
+  },
+  {
+    "revision": "501ce41595a9c3b62852b2e603559bff",
+    "url": "/static/media/background.501ce415.png"
+  },
+  {
+    "revision": "3ec746e6f492dead5ae7c3891ac99594",
+    "url": "/static/media/catherine-marsten.3ec746e6.jpg"
+  },
+  {
+    "revision": "ffaa4ce3a408a43a2059301b840f25d9",
+    "url": "/static/media/frederick_vondy.ffaa4ce3.jpg"
+  },
+  {
+    "revision": "bd1cf719734ffe49e122d35cda8d0e19",
+    "url": "/static/media/kay_adrian.bd1cf719.jpg"
+  },
+  {
+    "revision": "0f33d12ad7066d392998dbab2485a4c5",
+    "url": "/static/media/loading.0f33d12a.gif"
+  },
+  {
+    "revision": "30e48c5b4d13ceec1c714c54dfe611a1",
+    "url": "/static/media/logo.30e48c5b.png"
+  },
+  {
+    "revision": "465332d663b14d681998945a2c5051dc",
+    "url": "/static/media/slide1.465332d6.jpg"
+  }
+]);
